f6705c393df1958b82768f2602fa2a42891e91b1
HeeJae Chang <hechang@microsoft.com>
2023-11-28 17:11:15 -0800

    more code action examples

--- testing/single/src/codeaction.py
+++ testing/single/src/codeaction.py
@@ -24,3 +24,36 @@ MyType
 import sys
 
 
+# place cursor on `TypeToMove` and confirm lightbulb shows up
+# and trigger quick fix and confirm `move symbol` is listed
+# execute the code action and confirm it works as expected
+# and undo to make sure undo works as expected
+class TypeToMove:
+    pass
+
+
+# place cursor on `outerModule` and confirm lightbulb shows up
+# and trigger quick fix and confirm `Add "./outsideLib" to extraPaths` is listed
+# execute the code action and confirm `extraPaths` is added to `settings.json`
+import outerModule
+
+
+# place cursor on `hello` and confirm lightbulb shows up
+# and trigger quick fix and confirm `type ignore` entry is listed
+# execute the code action and confirm it added #type: ignore
+a: int = "hello"
+
+
+# place cursor on `unknownModule` and confirm lightbulb shows up
+# and trigger quick fix and confirm `select a different interpreter` and 
+# `learn more about resolving import` entry is listed
+# execute the code action and confirm it works as expected
+import unknownModule
+
+
+# select code between `codeToExtract` and `print(codeToExtract)` and confirm lightbulb shows up
+# and trigger quick fix and confirm `extract method` is listed
+# execute the code action and confirm it works as expected
+def function():
+    codeToExtract = 1
+    print(codeToExtract)
