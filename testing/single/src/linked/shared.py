72aeed0aa2b17d72e9d0d185a4a6dc885209ea07
HeeJae Chang <hechang@microsoft.com>
2023-12-01 16:56:58 -0800

    added sanity check test for symlink case

---
+++ testing/single/src/linked/shared.py
@@ -0,0 +1,12 @@
+# test file for symlink test
+"""
+    symbolic linked doc comment
+"""
+class SharedType:
+    """
+        shared type doc comment
+    """
+    pass
+
+class RenameType: 
+    pass
