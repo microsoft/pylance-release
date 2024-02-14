d20c6ddacdae4e39eab9f59d213ce319df7982e6
Rich Chiodo <rchiodo@users.noreply.github.com>
2024-02-13 17:29:33 -0800

    Add loose file test (#5468)

---
+++ testing/loose/importer.py
@@ -0,0 +1,5 @@
+# Open this file by itself without opening a workspace/folder.
+
+import exporter # Ensure there is no warning/error on this line
+
+exporter.foo() # Ensure there is a type definition for foo
