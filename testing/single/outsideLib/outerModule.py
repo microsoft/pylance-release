7dbfab602c00fffc9ca561c4067d4be8e5bdd3ba
Rich Chiodo <rchiodo@users.noreply.github.com>
2024-02-28 11:17:11 -0800

    Add testing for multiroot and unicode characters (#5535)
    * Add testing for multiroot
    
    * Add extra comments

--- testing/single/outsideLib/outerModule.py
+++ testing/single/outsideLib/outerModule.py
@@ -1,1 +1,3 @@
 # empty
+def outermodule_func():
+    pass
---
+++ testing/utfinname日本/reference_other_folder.py
@@ -0,0 +1,5 @@
+import outerModule
+
+# Verify there is no warning for `outerModule`
+# verify you get completions for outerModule (the outerModule path should be in the extraPaths for this folder)
+outerModule.
