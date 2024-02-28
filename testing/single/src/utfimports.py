f6ed021991361f417ec5d240ed5f7260905e3886
Rich Chiodo false <rchiodo@microsoft.com>
2024-02-28 10:51:17 -0800

    Add testing for multiroot

---
+++ testing/single/src/utfimports.py
@@ -0,0 +1,4 @@
+# In a multiroot workspace, this should find the 日本 module (Kanji for Nihon)
+
+import 日本
+日本.ni
---
+++ testing/utfinname日本/日本.py
@@ -0,0 +1,7 @@
+# This method should be referrable from the 'single' folder
+def nihon(): 
+    pass
+
+# Add this other method and make sure it shows up in completions in the single/src/utfimports.py 
+# def nihon_2():
+#    pass
