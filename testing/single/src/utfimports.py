fdb23a53bb09cce4fdf779a2d93c242ca0192c34
Rich Chiodo false <rchiodo@microsoft.com>
2024-02-28 10:56:20 -0800

    Add extra comments

--- testing/single/src/utfimports.py
+++ testing/single/src/utfimports.py
@@ -1,4 +1,5 @@
 # In a multiroot workspace, this should find the 日本 module (Kanji for Nihon)
+# otherwise if you just open the single folder, there should be an import resolution error below
 
 import 日本
-日本.ni
+日本.
