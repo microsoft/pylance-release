1bd626ebab4d647d55eca603d0d938c7526e42e6
Erik De Bonte <erikd@microsoft.com>
2024-03-28 16:49:01 -0700

    Update some comments in `testing\single` (#5708)

--- testing/single/src/utfimports.py
+++ testing/single/src/utfimports.py
@@ -1,4 +1,4 @@
-# In a multiroot workspace, this should find the 日本 module (Kanji for Nihon)
+# In a multiroot workspace (open `testing/test.code-workspace`), this should find the 日本 module (Kanji for Nihon)
 # otherwise if you just open the single folder, there should be an import resolution error below
 
 import 日本
