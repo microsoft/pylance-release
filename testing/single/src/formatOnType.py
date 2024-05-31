64a9df2bb1dc13ee63b5a18398f9f822457686d1
rchiodo <rchiodo@microsoft.com>
2024-05-31 17:48:31 +0000

    Change PR to add a comment explaining mode

--- testing/single/src/formatOnType.py
+++ testing/single/src/formatOnType.py
@@ -1,5 +1,7 @@
 # you can trigger format on type by hitting enter at the end of statement
-# Run this test twice, once with "editor.autoIndent" set to the default and once with "editor.autoIndent" set to "none"
+# This test assumes `editor.autoIndent` is set to 'none'. This is to ensure
+# the outcomes described are consistent. If you want to do ad-hoc testing, you should
+# change this value back to the default.
 
 # place cursor after `:` and hit enter and confirm cursor is placed
 # at the expected indentation
@@ -22,12 +24,6 @@ if ch == "a":
     pass
     # type `:` after `else` and see `else` is moved to right position.
     else
-
-# place cursor in the middle of this comment
-# if autoIndent is set to none, new line should not have comment, else it should
-if True:
-    # Try the same with this comment.
-    pass
 
 # Set "autoFormatStrings" to true and add a { into the string below. Make sure
 # it auto adds the `f` on the front. Repeat for the other strings
