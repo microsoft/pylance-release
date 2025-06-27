f13de31c23dec842ad7e411d1dc44b662631ad16
cdwhq <restrealm@outlook.com>
2025-06-27 11:24:39 -0400

    Delete testing directory

--- testing/single/src/utfimports.py
+++
@@ -1,5 +0,0 @@
-# In a multiroot workspace (open `testing/test.code-workspace`), this should find the 日本 module (Kanji for Nihon)
-# otherwise if you just open the single folder, there should be an import resolution error below
-
-import 日本
-日本.
